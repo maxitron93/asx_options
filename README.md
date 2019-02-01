e4ae834bbfdbd4e6f072653a4669905edeca3c6f
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-02-01 18:05:43 +1100

    Fix broken tickers and update README.md

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ This scraper creates a CSV file that contains the publicly available options dat
 5. `cd` into /asx_options-master and run `python -m scrapy crawl asx_spider`. This will create a .html file in /asx_options-master for every company that has ASX traded options. About 100 .html files will be generated.
 6. Run `python parser.py` from /asx_options-master. parser.py will create a new CSV file, parse through the .html files one by one, extract the options data, and place the data into the CSV file. This will take ~20 seconds.
 7. Copy-paste the data from the CSV file into a MS Excel spreadsheet.
+
+### Screenshot of the output - showing the the first ~50 of 43,837 lines in the csv file
+![First fifty lines of the CSV file](screenshot.png)
--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import csv
 
 all_companies = []
-company_codes = ["AGL", "ALL", "AM8", "AMC", "AMP", "AN8", "ANN", "ANZ", "AP8", "APA", "AR8", "ASX", "AWC", "AZJ", "BEN", "BH8", "BHP", "BLD", "BOQ", "BSL", "BX8", "BXB", "CB8", "CBA", "CCL", "CIM", "CPU", "CS8", "CSL", "CSR", "CTX", "CU8", "CWN", "CYB", "DX8", "FLT", "FM8", "FMG", "FXJ", "GM8", "GMG", "GP8", "GPT", "HVN", "IA8", "IAG", "IFL", "IL8", "ILU", "IPL", "JH8", "JHX", "LL8", "LLC", "MG8", "MGR", "MPL", "MQ8", "MQG", "MTS", "MYR", "NA8", "NAB", "NC8", "NCM", "OI8", "OR8", "ORG", "ORI", "OS8", "OSH", "OZL", "QA8", "QAN", "QBE", "RH8", "RHC", "RI8", "RIO", "RRL", "S32", "SC8", "SCG", "SEK", "SG8", "SGM", "SGP", "SGR", "SHL", "ST8", "STO", "STW", "SUN", "SY8", "SYD", "TAH", "TC8", "TCL", "TL8", "TLS", "TW8", "TWE", "VC8", "VCX", "WB8", "WBC", "WES", "WF8", "WFD", "WO8", "WOR", "WOW", "WPL", "XJO"]
+company_codes = ["AGL", "ALL", "AM8", "AMC", "AMP", "AN8", "ANN", "ANZ", "AP8", "APA", "AR8", "ASX", "AWC", "AZJ", "BEN", "BH8", "BHP", "BLD", "BOQ", "BSL", "BX8", "BXB", "CB8", "CBA", "CCL", "CIM", "CPU", "CS8", "CSL", "CSR", "CTX", "CU8", "CWN", "CYB", "DX8", "FLT", "FM8", "FMG", "GM8", "GMG", "GP8", "GPT", "HVN", "IA8", "IAG", "IFL", "IL8", "ILU", "IPL", "JH8", "JHX", "LL8", "LLC", "MG8", "MGR", "MPL", "MQ8", "MQG", "MTS", "MYR", "NA8", "NAB", "NC8", "NCM", "OI8", "OR8", "ORG", "ORI", "OS8", "OSH", "OZL", "QA8", "QAN", "QBE", "RH8", "RHC", "RI8", "RIO", "RRL", "S32", "SC8", "SCG", "SEK", "SG8", "SGM", "SGP", "SGR", "SHL", "ST8", "STO", "STW", "SUN", "SY8", "SYD", "TAH", "TC8", "TCL", "TL8", "TLS", "TW8", "TWE", "VC8", "VCX", "WB8", "WBC", "WES", "WO8", "WOR", "WOW", "WPL", "XJO"]
 
 for company_code in company_codes:
     with open("Options-%s.html" % company_code) as html_file:
